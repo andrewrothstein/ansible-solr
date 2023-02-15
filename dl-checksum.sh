1be7ab06e686541cb04707fb735e53304060bb11
Andrew Rothstein <andrew.rothstein@gmail.com>
2023-02-15 10:57:06 -0500

    solr:9.1.1

--- dl-checksum.sh
+++ dl-checksum.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env sh
 DIR=~/Downloads
-MIRROR=https://www.apache.org/dist/lucene/solr
-
+MIRROR=https://downloads.apache.org/solr/solr
+# https://downloads.apache.org/solr/solr/9.1.1/solr-9.1.1.tgz.sha512
 dl_ver() {
     local ver=$1
     local shasum=solr-${ver}.tgz.sha512
@@ -11,4 +11,4 @@ dl_ver() {
     printf "  '%s': sha512:%s\n" $ver $(curl -sSLf $url | awk '{print $1}')
 }
 
-dl_ver ${1:-8.11.2}
+dl_ver ${1:-9.1.1}
